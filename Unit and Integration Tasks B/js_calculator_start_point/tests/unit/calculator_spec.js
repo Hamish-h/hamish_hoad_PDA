fb1b938c897165a24f7ce0c7e239fcef2b11fbd2
Hamish-H <hamish.h@outlook.com>
2018-07-23 10:15:57 +0100

    added unit test multiplication

---
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,25 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function () {
+    assert.equal(true, true)
+  })
+
+  // ####The program should correctly perform the following tasks:
+
+  // multiply 3x5 and get 15
+
+  it('it should multiply values, 3 times 5 to return 15', function () {
+    calculator.previousTotal = 0;
+    calculator.operatorClick(calculator.add(3));
+    calculator.operatorClick(calculator.multiply(5));
+    assert.strictEqual(calculator.runningTotal, 15)
+  });
+
+});
