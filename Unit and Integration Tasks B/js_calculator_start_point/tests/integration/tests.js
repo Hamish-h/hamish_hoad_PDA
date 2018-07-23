5211a4a6f14818048de43c5618142bea206c8261
Hamish-H <hamish.h@outlook.com>
2018-07-23 11:09:37 +0100

    added integration tasks

--- Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
@@ -16,12 +16,29 @@ describe('calculator functionality', function () {
     browser.get('http://localhost:3000')
   });
 
+  // Do the number buttons work to update the display of the running total ? 
 
   it('should have working number buttons', function () {
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
+
+  // Do each of the arithmetical operations work to update the display with the result of the operation ? 
+
+  // Can we chain multiple operations together ? 
+
+  // Does it work as expected for a range of numbers ? (positive, negative, decimals, large numbers) 
+
+  //What does the code do in exceptional circumstances ? 
+
+  //If you divide by zero, what is the effect ? 
+  // Can you write a test to describe what you'd prefer to happen under this circumstance, 
+  // and then correct to code to make that test pass.
+
+
+
+
 
 
 
