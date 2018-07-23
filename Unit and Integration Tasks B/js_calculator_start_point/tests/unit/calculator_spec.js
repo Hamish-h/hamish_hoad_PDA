c077b8481f63130ff1648b1e9e02951e9941cc4d
Hamish-H <hamish.h@outlook.com>
2018-07-23 10:17:14 +0100

    removed the sample test

--- Unit and Integration Tasks B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -7,14 +7,10 @@ describe('calculator', function () {
   });
 
   // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function () {
-    assert.equal(true, true)
-  })
 
   // ####The program should correctly perform the following tasks:
 
   // multiply 3x5 and get 15
-
   it('it should multiply values, 3 times 5 to return 15', function () {
     calculator.previousTotal = 0;
     calculator.operatorClick(calculator.add(3));
